b42639f3cbb2b9e2e73eda160bd98ef3c84fd604
Niaolan27 <jasonniow.work@gmail.com>
2023-09-15 21:13:52 -0400

    updated dim reduction, error with magnitude of reduced vectors

--- server/sentenceTransform.py
+++ server/sentenceTransform.py
@@ -1,6 +1,9 @@
 from sentence_transformers import SentenceTransformer, util
 #import umap
 from sklearn.decomposition import PCA
+import numpy
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
 
 def returnEmbedding(sentence):
     model = SentenceTransformer('all-MiniLM-L6-v2')
@@ -9,30 +12,22 @@ def returnEmbedding(sentence):
     return embedding
 
 def reduceDimensions(embedding):
+    #print(embedding)
+    #print(embedding.reshape(1,-1))
+
+    #scaling the data (not sure if this helps, GPT says it helps )
+    scaler = StandardScaler()
+    scaled_embedding = scaler.fit_transform(embedding.reshape(-1,1)).reshape(384,)
+    #print(scaled_embedding)
+
     # Define the desired output dimension (e.g., reduce to 128 dimensions)
+    #stacking 3 embeddings because PCA requires number of samples to be at least equal to number of components(i.e. 3)
+    embeddings = numpy.array([scaled_embedding, scaled_embedding, scaled_embedding])
     output_dimension = 3
-    print(embedding.shape)
-    # Initialize and fit PCA to reduce dimensionality
     pca = PCA(n_components=output_dimension)
-    reduced_embedding = pca.fit_transform(embedding.reshape(384,1))
+    reduced_embedding = pca.fit_transform(embeddings)
     print(reduced_embedding)
     return reduced_embedding
 
 originalEmbedding = returnEmbedding('I am Jason.')
 reducedEmbedding = reduceDimensions(originalEmbedding)
-
-# similarity_score_model1_01 = util.pytorch_cos_sim(embeddings_1[0], embeddings_1[1])
-# similarity_score_model1_02 = util.pytorch_cos_sim(embeddings_1[0], embeddings_1[2])
-# similarity_score_model1_03 = util.pytorch_cos_sim(embeddings_1[0], embeddings_1[3])
-# similarity_score_model1_12 = util.pytorch_cos_sim(embeddings_1[1], embeddings_1[2])
-# similarity_score_model1_13 = util.pytorch_cos_sim(embeddings_1[1], embeddings_1[3])
-# similarity_score_model1_23 = util.pytorch_cos_sim(embeddings_1[2], embeddings_1[3])
-# print(f"Similarity score for Model 1 between 0 and 1: {similarity_score_model1_01.item()}")
-# print(f"Similarity score for Model 1 between 0 and 2: {similarity_score_model1_02.item()}")
-# print(f"Similarity score for Model 1 between 0 and 3: {similarity_score_model1_03.item()}")
-# print(f"Similarity score for Model 1 between 1 and 2: {similarity_score_model1_12.item()}")
-# print(f"Similarity score for Model 1 between 1 and 3: {similarity_score_model1_13.item()}")
-# print(f"Similarity score for Model 1 between 2 and 3: {similarity_score_model1_23.item()}")
-
-# print("--- %s seconds ---" % (time.time() - start_time))
-
