b12d8ae90e30b2d773e2c7216627947160daad3c
mycoalchen <chen.michael.1116@gmail.com>
2023-09-15 21:26:22 -0400

    updated point styling

--- frontend/src/PointCloud.js
+++ frontend/src/PointCloud.js
@@ -1,53 +1,104 @@
 import React, { useRef, useState } from 'react';
 import { Canvas } from 'react-three-fiber';
+import * as THREE from 'three';
 import { animated, a } from '@react-spring/three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { useSpring } from 'react-spring';
 
-function Point({ position, color }) {
-  const [hovered, setHovered] = useState(false);
+const vertexShader = `
+  varying vec3 vPosition;
+  void main() {
+    vPosition = position;
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+  }
+`;
+
+const fragmentShader = `
+  varying vec3 vPosition;
+  uniform float hoverIntensity; // Intensity of the hover effect
+
+  void main() {
+    vec3 coreColor = vec3(1.0, 1.0, 1.0); // White core color
+
+    // Calculate the distance from the center of the point
+    float distance = length(gl_PointCoord - vec2(0.5, 0.5));
+    
+    // Define the size of the white core and the glow
+    float coreSize = 0.1; // Adjust the size of the white core
+    float glowSize = 0.3; // Adjust the size of the glow
+
+    // Create a smooth transition from the core to the glow
+    float alpha = smoothstep(coreSize, coreSize + glowSize, distance);
+
+    // Mix the core color and the glow color based on the distance and hover intensity
+    vec3 finalColor = mix(coreColor, coreColor * (1.0 + hoverIntensity), alpha);
+
+    gl_FragColor = vec4(finalColor, 1.0);
+  }
+`;
+
+function Point({ position, onHover }) {
+  const [isHovered, setHovered] = useState(false);
+
+  const { hoverIntensity } = useSpring({
+    hoverIntensity: isHovered ? 0.5 : 0.0, // Adjust the hover intensity as needed
+  });
 
   return (
-    <animated.mesh // Use 'animated.mesh' here
+    <animated.mesh
       position={position}
       onClick={() => console.log('Clicked!')}
-      onPointerOver={() => setHovered(true)}
-      onPointerOut={() => setHovered(false)}
-      scale={hovered ? [1.2, 1.2, 1.2] : [1, 1, 1]} // Use 'animated' for scaling
+      onPointerOver={() => {
+        setHovered(true);
+        onHover(true);
+      }}
+      onPointerOut={() => {
+        setHovered(false);
+        onHover(false);
+      }}
     >
       <sphereGeometry args={[0.1, 32, 32]} />
-      <meshBasicMaterial color={color} />
+      <shaderMaterial
+        attach="material"
+        args={[{ vertexShader, fragmentShader, uniforms: { hoverIntensity: hoverIntensity } }]}
+      />
     </animated.mesh>
   );
 }
 
 function PointCloud() {
+  const points = [];
 
+  for (let i = 0; i < 100; i++) {
+    const theta = Math.random() * Math.PI; // [0, π]
+    const phi = Math.random() * 2 * Math.PI; // [0, 2π]
 
-  const points = [
-    { position: [5, 0, 0], color: 'red' },
-    { position: [1, 1, 1], color: 'blue' },
-    { position: [3, 2, 0], color: 'green' },
-    // Add more points with positions and colors as needed
-  ];
-  for (var i = 0; i < 100; i++) {
-    points.push({position: [Math.random()*5, Math.random()*5, Math.random()*5], color: 
-    'white'})
+    const radius = 10;
+    const x = radius * Math.sin(theta) * Math.cos(phi);
+    const y = radius * Math.sin(theta) * Math.sin(phi);
+    const z = radius * Math.cos(theta);
+
+    points.push({ x, y, z });
   }
 
   return (
-    <Canvas
-      camera={{ position: [0, 0, 5] }}
-      style={{ background: 'black', height: '100%', width: '100%'}}
-      onCreated={({ gl, camera }) => {
-        const controls = new OrbitControls(camera, gl.domElement);
-        controls.minDistance = 1;
-        controls.maxDistance = 10;
-      }}
-    >
-      {points.map((point, index) => (
-        <Point key={index} position={point.position} color={point.color} />
-      ))}
-    </Canvas>
+    <div style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%' }}>
+      <Canvas
+        camera={{ position: [0, 0, 0], fov: 90 }}
+        onCreated={({ gl, camera, scene }) => {
+          const controls = new OrbitControls(camera, gl.domElement);
+          controls.minDistance = 1;
+          controls.maxDistance = 10;
+        }}
+        width="100%"
+        height="100%"
+        style={{ background: 'black' }}
+      >
+        {points.map((point, index) => (
+          <Point key={index} position={point} onHover={(isHovered) => console.log('Hovered:', isHovered)} />
+        ))}
+      </Canvas>
+    </div>
   );
 }
 
