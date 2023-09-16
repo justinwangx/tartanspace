84f86a239533d7cda10231d93c39d69aab611c36
Justin Wang <justinwang46@gmail.com>
2023-09-15 22:05:21 -0400

    form submission

--- frontend/src/FormPage.js
+++ frontend/src/FormPage.js
@@ -24,7 +24,29 @@ function FormPage() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log('Submitted:', formData);
+    
+    const response = await fetch("http://ec2-44-196-61-225.compute-1.amazonaws.com:8080/form-submission", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(formData)
+    });
+
+    if (response.ok) {
+      // Clear the form if the submission is successful
+      setFormData({
+        firstName: "",
+        lastName: "",
+        email: "",
+        single: false,
+        question1: "",
+        question2: "",
+        question3: "",
+        question4: "",
+        question5: ""
+      });
+    }
   };
 
   return (
@@ -34,25 +56,25 @@ function FormPage() {
         <form onSubmit={handleSubmit} className="space-y-4">
           <div className="flex flex-col">
             <label htmlFor="firstName">First Name:</label>
-            <input type="text" id="firstName" name="firstName" value={formData.firstName} onChange={handleChange} className="border p-2" />
+            <input type="text" id="firstName" name="firstName" value={formData.firstName} onChange={handleChange} className="border p-2" required />
           </div>
           <div className="flex flex-col">
             <label htmlFor="lastName">Last Name:</label>
-            <input type="text" id="lastName" name="lastName" value={formData.lastName} onChange={handleChange} className="border p-2" />
+            <input type="text" id="lastName" name="lastName" value={formData.lastName} onChange={handleChange} className="border p-2" required />
           </div>
           <div className="flex flex-col">
             <label htmlFor="email">Email:</label>
-            <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} className="border p-2" />
+            <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} className="border p-2" required />
           </div>
           {[1, 2, 3, 4, 5].map((i) => (
             <div className="flex flex-col" key={i}>
               <label htmlFor={`question${i}`}>Question {i}:</label>
-              <input type="text" id={`question${i}`} name={`question${i}`} value={formData[`question${i}`]} onChange={handleChange} className="border p-2" />
+              <input type="text" id={`question${i}`} name={`question${i}`} value={formData[`question${i}`]} onChange={handleChange} className="border p-2" required />
             </div>
           ))}
           <div className="flex flex-col">
             <label htmlFor="single">Single:</label>
-            <input type="checkbox" id="single" name="single" checked={formData.single === 'yes'} onChange={() => setFormData({ ...formData, single: formData.single === 'yes' ? 'no' : 'yes' })} />
+            <input type="checkbox" id="single" name="single" checked={formData.single === 'yes'} onChange={() => setFormData({ ...formData, single: formData.single === 'yes' ? 'no' : 'yes' })} required />
           </div>
           <button type="submit" className="w-full bg-blue-500 text-white p-2 rounded">Submit</button>
         </form>
