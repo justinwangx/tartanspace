09fda90ca08400c076519abcf0b7ff309fbd780a
Justin Wang <justinwang46@gmail.com>
2023-09-16 11:37:49 -0400

    styling

--- frontend/src/About.js
+++ frontend/src/About.js
@@ -4,32 +4,32 @@ import { Link } from "react-router-dom";
 const About = () => {
   return (
     <div w-full h-screen>
-      <div className="flex flex-col justify-center items-center mt-20">
+      <div className="flex flex-col justify-center items-center mt-20 font-mooli text-red-500">
         <div className="w-5/12 leading-8">
-          <p>
+          <p className="mb-2">
             tartanspace is a tool designed to connect students at Carnegie
             Mellon.
           </p>
-          <p>
+          <p className="mb-2">
             Each member of tartanspace completes a form containing pesonal
             questions that reveal information about themselves.
           </p>
-          <p>
+          <p className="mb-2">
             Text responses from the form are then embedded into high-dimensional
             space using transformer-based language models.
           </p>
-          <p>
+          <p className="mb-2">
             These embeddings are then mapped to 3-dimensional space using a
             dimensionality reduction algorithm.
           </p>
-          <p>
+          <p className="mb-2">
             People with similar responses appear closer together in 3D space.
           </p>
-          <p>
+          <p className="mb-2">
             tartanspace will be open until __, at which point the form will stop
             accepting responses.
           </p>
-          <p>
+          <p className="mb-2">
             Then members will be emailed the names and contact information of
             their 3 closest neighbors in 3D space.
           </p>
