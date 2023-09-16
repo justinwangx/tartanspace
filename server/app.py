7d2cb21c632ef3b61cd07c7988e2ccfec72aa29b
server <justinwang46@gmail.com>
2023-09-16 03:31:52 +0000

    embed form response

--- server/app.py
+++ server/app.py
@@ -1,16 +1,35 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+from embed_and_reduce import get_embedding, reduce_dimensions
+
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return "tartanspace"
 
-@app.route('/embed', methods=['POST'])
-def embed_text():
-    text = request.json.get('text')
-    # Embed the text and perform UMAP, etc.
-    # embedding = your_embedding_function(text)
-    return jsonify(embedding=embedding)
+@app.route('/form-submission', methods=['POST'])
+def process_submission():
+    first_name = request.json.get("firstName")
+    last_name = request.json.get("lastName")
+    email = request.json.get("email")
+    answer1 = request.json.get("question1")
+    answer2 = request.json.get("question2")
+    answer3 = request.json.get("question3")
+    answer4 = request.json.get("question4")
+    answer5 = request.json.get("question5")
+    answers = [answer1, answer2, answer3, answer4, answer5]
+
+    # embed text
+    embedding = get_embedding(answers)
+
+    return jsonify(name="success")
+
+@app.route('/get-points', methods=['GET'])
+def get_points():
+    return None
 
 @app.route('/test-endpoint', methods=['GET'])
 def your_endpoint():
@@ -23,3 +42,6 @@ def your_endpoint():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
+
+# for now:
+# FLASK_APP=app.py FLASK_ENV=development flask run --host=0.0.0.0 --port=8080
--- server/embed_and_reduce.py
+++ server/embed_and_reduce.py
@@ -1,28 +1,27 @@
-from sentence_transformers import SentenceTransformer, util
-#import umap
-from sklearn.decomposition import PCA
 import numpy as np
+from sentence_transformers import SentenceTransformer, util
+
+from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import MinMaxScaler
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.pyplot as plt
 
+# from mpl_toolkits.mplot3d import Axes3D
+# import matplotlib.pyplot as plt
 
-
-def returnEmbedding(group_sentences):
+def get_embedding(sentences: list):
     model = SentenceTransformer('all-MiniLM-L6-v2')
-    group_embeddings = [model.encode(group) for group in group_sentences]
-    mean_embeddings = np.mean(group_embeddings, axis=0)
-    return mean_embeddings
+    embeddings = model.encode(sentences)
+    mean_embedding = np.mean(embeddings, axis=0)
+    return mean_embedding
 
-def reduceDimensions(embeddings):
+def reduce_dimensions(embeddings):
     output_dimension = 3
     pca = PCA(n_components=output_dimension)
     reduced_embedding = pca.fit_transform(embeddings)
     return reduced_embedding
-    
-if __name__ == "__main__":
-    # Open the file in read mode ('r')
+
+def test_on_fake_data():
+# Open the file in read mode ('r')
     # Read the content of the text file
     with open('samplePersons.txt', 'r') as file:
         lines = file.readlines()
@@ -45,13 +44,13 @@ if __name__ == "__main__":
     embedding_list = np.empty((0, 384))  # Assuming you want to append arrays of shape (384,)
 
     for person in interests_data:
-        embedding = returnEmbedding(person)
+        embedding = get_embedding(person)
         #print(embedding.shape)
         embedding_list = np.vstack((embedding_list, embedding))  # Append the embedding as a new row
         #print(embedding_list.shape)
     print(embedding_list.shape)
     # Reduce dimensions using PCA
-    reduced_embeddings = reduceDimensions(embedding_list)
+    reduced_embeddings = reduce_dimensions(embedding_list)
 
     # print(reducedEmbedding)
 
@@ -64,28 +63,6 @@ if __name__ == "__main__":
     ax.set_zlabel('Principal Component 3')
     plt.title('PCA Reduced Data (3D)')
     plt.show()
-
-
-    # # reducedEmbedding = reduceDimensions(originalEmbedding)
-
-    # # Split sentences into groups of 7
-    # grouped_sentences = [sentences[i:i+7] for i in range(0, len(sentences), 7)]
-
-    # # Calculate embeddings for each group
-    # group_embeddings = [returnEmbedding(group) for group in grouped_sentences]
-
-    # # Calculate the mean embedding for each group
-    # mean_embeddings = [np.mean(group, axis=0) for group in group_embeddings]
-
-    # # Reduce dimensions using PCA
-    # reduced_embeddings = reduceDimensions(mean_embeddings)
-
-    # # print(reducedEmbedding)
-    # fig = plt.figure()
-    # ax = fig.add_subplot(111, projection='3d')
-    # ax.scatter(reduced_embeddings[:, 0], reduced_embeddings[:, 1], reduced_embeddings[:, 2])
-    # ax.set_xlabel('Principal Component 1')
-    # ax.set_ylabel('Principal Component 2')
-    # ax.set_zlabel('Principal Component 3')
-    # plt.title('PCA Reduced Data (3D)')
-    # plt.show()
+    
+if __name__ == "__main__":
+   test_on_fake_data() 
