60bc0e7b831bde409261ce400ab1e11ddf980a83
Niaolan27 <jasonniow.work@gmail.com>
2023-09-15 21:21:46 -0400

    updated dim reduction

--- server/sentenceTransform.py
+++ server/sentenceTransform.py
@@ -11,18 +11,7 @@ def returnEmbedding(sentence):
     #print(embedding.shape)
     return embedding
 
-def reduceDimensions(embedding):
-    #print(embedding)
-    #print(embedding.reshape(1,-1))
-
-    #scaling the data (not sure if this helps, GPT says it helps )
-    scaler = StandardScaler()
-    scaled_embedding = scaler.fit_transform(embedding.reshape(-1,1)).reshape(384,)
-    #print(scaled_embedding)
-
-    # Define the desired output dimension (e.g., reduce to 128 dimensions)
-    #stacking 3 embeddings because PCA requires number of samples to be at least equal to number of components(i.e. 3)
-    embeddings = numpy.array([scaled_embedding, scaled_embedding, scaled_embedding])
+def reduceDimensions(embeddings):
     output_dimension = 3
     pca = PCA(n_components=output_dimension)
     reduced_embedding = pca.fit_transform(embeddings)
@@ -30,4 +19,5 @@ def reduceDimensions(embedding):
     return reduced_embedding
 
 originalEmbedding = returnEmbedding('I am Jason.')
-reducedEmbedding = reduceDimensions(originalEmbedding)
+x = numpy.random.rand(100, 384)  # 100 samples, 384 dimensions
+reducedEmbedding = reduceDimensions(x)
